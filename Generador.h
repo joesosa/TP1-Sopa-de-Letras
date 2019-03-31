186de256f50f10b54d722b286640d18f421b5f01
joesosa <44095239+joesosa@users.noreply.github.com>
2019-03-30 20:09:40 -0600

    Update Generador.h

--- Generador.h
+++ Generador.h
@@ -1,19 +1,23 @@
 #ifndef GENERADOR_H
 #define GENERADOR_H
-
+#include "Palabra.h"
+#include "Matriz.h"
 
 class Generador
 {
     public:
         Generador(int f, int c);
 
-        void ponerPalabra(int dir, int posF, int posC, char * palabra, int i);
-        void ponerPalabra(char * palabra);
+        void ponerPalabra(int, int, int, Palabra, int);
+        void ponerPalabra(char*);
+        void guardoPalabra(char*,int);
 
     private:
 		int f; 
 		int c; 
         Matriz m;
+        Palabra lista[];
+        int cuantasPalabras = 0;
         const int sumaF[8]={-1,-1,0,1,1,1,0,-1};
     	const int sumaC[8]={0,-1,-1,-1,0,1,1,1};
 };
