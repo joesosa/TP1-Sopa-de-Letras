5638eecd9b8b95f19ef143a086ef0b1a09ec0c4b
joesosa <44095239+joesosa@users.noreply.github.com>
2019-03-30 20:08:40 -0600

    Update Generador.cpp

--- Generador.cpp
+++ Generador.cpp
@@ -11,30 +11,34 @@ Generador::Generador(int f, int c){
 }
 
 
-void Generador::ponerPalabra(int dir, int posF, int posC, char * palabra, int i){
-	Palabra p(palabra); 
+void Generador::ponerPalabra(int dir, int posF, int posC, Palabra p, int i){
     if(m.posicionVacia(posF, posC)){
-           if (i< p.getLength()){
-            m.setValor(posF, posC, p.charAt(i));
+           if (i< lista[cuantasPalabras].getLength()){
+            m.setValor(posF, posC, lista[cuantasPalabras].charAt(i));
 			m.imprimir(cout); 
-            ponerPalabra(dir, posF+sumaF[dir], posC+sumaC[dir], p, i++);
+            ponerPalabra(dir, posF+sumaF[dir], posC+sumaC[dir], lista[cuantasPalabras], i++);
            }
     }
     else{
-            if(i>0 && i < p.getLength()-1){
+            if(i>0 && i < lista[cuantasPalabras].getLength()-1){
                 m.quitarValor(posF, posC);
-                ponerPalabra(dir, posF-sumaF[dir], posC-sumaC[dir], p, i--);
+                ponerPalabra(dir, posF-sumaF[dir], posC-sumaC[dir], lista[cuantasPalabras], i--);
             }
 
     }
 }
 
+void Generador::guardoPalabra(char * ptr, int n){
+    Palabra * pl = new Palabra(ptr);
+        lista[n] = *pl;   
+}
+
 void Generador::ponerPalabra(char * palabra){
-    int dir= 1 + rand() % 8;
+    int dir = 1 + rand() % 8;
     int posF = rand() % f;
     int posC = rand() % c;
     int i = 0;
-    ponerPalabra(dir, posF, posC, char * palabra, i);
+    ponerPalabra(dir, posF, posC, palabra, i);
 
 }
 
