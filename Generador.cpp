db07d3846e8c2a692ad7c5b81e3419af3df47ff6
joesosa <44095239+joesosa@users.noreply.github.com>
2019-03-31 19:05:19 -0600

    Update Generador.cpp

--- Generador.cpp
+++ Generador.cpp
@@ -13,11 +13,11 @@ Generador::Generador(int f, int c){
 
 void Generador::ponerPalabra(int dir, int posF, int posC, Palabra p, int i){
     if(m.posicionVacia(posF, posC)){
-           if (i< lista[cuantasPalabras].getLength()){
+        if(i< lista[cuantasPalabras].getLength()){
             m.setValor(posF, posC, lista[cuantasPalabras].charAt(i));
 			m.imprimir(cout); 
             ponerPalabra(dir, posF+sumaF[dir], posC+sumaC[dir], lista[cuantasPalabras], i++);
-           }
+        }
     }
     else{
             if(i>0 && i < lista[cuantasPalabras].getLength()-1){
@@ -33,13 +33,87 @@ void Generador::guardoPalabra(char * ptr, int n){
         lista[n] = *pl;   
 }
 
-void Generador::ponerPalabra(char * palabra){
-    int dir = 1 + rand() % 8;
+void Generador::ponerPalabra(Palabra palabra){
+    int dir = palabra.getPosicion();
     int posF = rand() % f;
     int posC = rand() % c;
     int i = 0;
     ponerPalabra(dir, posF, posC, palabra, i);
 
+}
+
+void Generador::rellenaSopa(){
+    int rand = 0;
+    for(int f = 0; f < this->f;++f){
+        for(int c = 0; c < this->c;++c){
+            if(m.getValor(f,c) ==  '_'){
+                rand = 1 + rand % 26;
+                m.setValor(f,c,letraAleatoria(rand));
+            }
+        }
+    }
+}
+
+char Generador::letraAleatoria(int n){
+    char devuelvo = '\0';
+    switch (n)
+    {
+        case 1: devuelvo = 'a';
+            break;
+        case 2: devuelvo = 'b';
+            break;
+        case 3: devuelvo = 'c';
+            break;
+        case 4: devuelvo = 'd';
+            break;
+        case 5: devuelvo = 'e';
+            break;
+        case 6: devuelvo = 'f';
+            break;
+        case 7: devuelvo = 'g';
+            break;
+        case 8: devuelvo = 'h';
+            break;
+        case 9: devuelvo = 'i';
+            break;
+        case 10: devuelvo = 'j';
+            break;
+        case 11: devuelvo = 'k';
+            break;
+        case 12: devuelvo = 'l';
+            break;
+        case 13: devuelvo = 'm';
+            break;
+        case 14: devuelvo = 'n';
+            break;
+        case 15: devuelvo = 'o';
+            break;
+        case 16: devuelvo = 'p';
+            break;
+        case 17: devuelvo = 'q';
+            break;
+        case 18: devuelvo = 'r';
+            break;
+        case 19: devuelvo = 's';
+            break;
+        case 20: devuelvo = 't';
+            break;
+        case 21: devuelvo = 'u';
+            break;
+        case 22: devuelvo = 'v';
+            break;
+        case 23: devuelvo = 'w';
+            break;
+        case 24: devuelvo = 'x';
+            break;
+        case 25: devuelvo = 'y';
+            break;
+        case 26: devuelvo = 'z';
+            break;
+        default:
+            break;
+    }
+    return devuelvo;
 }
 
 Generador::~Generador()
