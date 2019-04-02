0f98c161844a3141573207d2af8dd439ad534672
joesosa <44095239+joesosa@users.noreply.github.com>
2019-04-02 01:04:58 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,35 +2,36 @@
 #include<iostream>
 #include "Generador.h"
 using namespace std;
-// PARA PROBAR ESTE PROGRAMA
-// Ejecute en consola
+
 // g++ -o programa Main.cpp
-// programa Main.cpp MainConNumerosDeLinea.txt
+
 int main(int argc, char * * argv){
-   int filas,columnas,cantPalabras;
    
+   int filas,columnas,cantPalabras;
    if(argc > 4){ 
-        ifstream entrada(argv[1]);
+        ifstream entrada(argv[1]); //la entrada
 	     filas = atoi(argv[2]);
 	     columnas = atoi(argv[3]);
-        ofstream salida(argv[4]);
+        ofstream salida(argv[4]); //la salida
         char ** palabra;
         entrada>>cantPalabras;
-        palabra = new char * [cantPalabras];
+        salida << filas << " " << columnas << endl;
+        palabra = new char * [cantPalabras]; //se crea un vector de punteros a char
         Generador pal(filas,columnas,cantPalabras);
-        for(int i =0;i<cantPalabras;++i){
-            entrada >> palabra[i];
-            pal.guardoPalabra(palabra[i],cantPalabras++);
-         }
+        
+       // for(int i =0;i<cantPalabras;++i){ //almacena los punteros a char en el vector de palabras
+          //  entrada >> palabra[i];
+           // pal.guardoPalabra(palabra[i],i);
+         //}
          
-         salida << filas << "\0" << columnas << endl;
+         //salida << filas << "\0" << columnas << endl; //saca en el archico las filas y las columnas
+        
          for(int f = 0; f < filas;++f){
             for(int c = 0; c < columnas; ++c){
                salida << pal.getValor(f,c)<<"\0";
             }
             salida<<endl;
          }
-         
    } 
    else if(argc == 4){
         ifstream entrada(argv[0]);
@@ -40,18 +41,20 @@ int main(int argc, char * * argv){
         entrada>>cantPalabras;
         palabra = new char * [cantPalabras];
         Generador pal(filas,columnas,cantPalabras);
-        for(int i =0;i<cantPalabras;++i){
+        
+        /*for(int i =0;i<cantPalabras;++i){
             entrada >> palabra[i];
-            pal.guardoPalabra(palabra[i],cantPalabras++);
-         }
+            pal.guardoPalabra(palabra[i],i);
+         }*/
 
          cout<<filas<<"\0"<<columnas<<endl;
-         for(int f = 0; f < filas;++f){
+        
+         /*for(int f = 0; f < filas;++f){
             for(int c = 0; c < columnas; ++c){
                cout << pal.getValor(f,c)<<"\0";
             }
             cout<<endl;
-         }
+         }*/
    }
    else{
       cout<<"No se pasaron los suficientes parametros"<<endl;
