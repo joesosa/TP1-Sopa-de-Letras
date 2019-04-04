88f18ef6297e2669dcd40f51fc93a654e221338a
joesosa <44095239+joesosa@users.noreply.github.com>
2019-04-04 16:02:13 -0600

    Update Generador.cpp

--- Generador.cpp
+++ Generador.cpp
@@ -7,41 +7,38 @@ using namespace std;
 Generador::Generador(int f, int c, int n){
     this->f = f;
     this->c = c;
-    Matriz m(f, c);
+    m = new Matriz(f,c);
     lista = new Palabra [n]; 
    // m.rellenoSopa();
-    m.imprimir(cout); 
+    m->imprimir(cout); 
 }
-
-void Generador::guardoPalabra(char * ptr, int n){
-     m.imprimir(cout);
-    Palabra pl(ptr);
-    lista [n] = pl; 
-    cout << lista [n].getHilera() << endl; 
-    posicionPalabra(lista [n]); 
-    m.imprimir(cout);
-    m.rellenoSopa(); 
-    m.imprimir(cout); 
-}
-
-void Generador::posicionPalabra(Palabra p){
-    int dir = rand() % 8;
-    int posF = rand() % f;
-    int posC = rand() % c;
-
-    cout << p.getHilera() << endl;
-    cout << dir << "dir" << endl;
-    cout << posF << "posF" << endl;
-    cout << posC << "posC" << endl;
-    
-    m.ponerPalabra(dir, posF, posC, p);
-}
-
-
-
-
-
 
 Generador::~Generador(){
     delete [] lista;
+	delete m;
 }
+
+/**
+ * @Descripcion: almacena los * a char en el vector de palabras
+ * @Param: ptr: char* palabra
+ * @Param: n:int cantida de palabras
+ */
+void Generador::guardoPalabra(char * ptr, int n){
+    Palabra pl(ptr);
+    lista [n] = pl; 
+    posicionPalabra(lista [n]); 
+}
+
+/**
+ * @Descripcion: busca una posicion aleatoria en la matriz y la forma en que se va a colocar,
+ * y ademas llama al metodo encargado de llamar a los demas colocando una palabra en un posicion,
+ * de la matriz, con una direccion
+ * @Param: p: Palabra recibe una palabra
+ */
+void Generador::posicionPalabra(Palabra p){
+    int dir = p.getDireccion();
+    int posF = rand() % f;
+    int posC = rand() % c;
+	m->ponerPalabra(dir, posF, posC, p);
+}
+
